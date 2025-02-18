f511981cafa323cd2e61e7b3adea0e7daba7f0df
Random <liuboi@gmail.com>
2025-02-18 19:10:07 +0200

    Diena 2: Busenos logika

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,22 @@
+import { useState } from 'react';
 import { Image, SafeAreaView, StatusBar, Text, View } from 'react-native';
 import question from '../assets/data/oneQuestionWithOption';
 import ImageOption from '~/components/ImageOption';
+import CustomButton from '~/components/CustomButton';
+
+type Option = {
+  id: string;
+  image: string;
+  text: string;
+};
+
 export default function Home() {
+  const [selected, setSelected] = useState<Option | null>(null);
+
+  const onButtonPress = () => {
+    console.warn('Pasirinktas atsakymas', selected?.text);
+  };
+
   return (
     <SafeAreaView className="flex flex-1 items-center justify-center p-3">
       <StatusBar animated barStyle={'light-content'} />
@@ -9,9 +24,17 @@ export default function Home() {
 
       <View className="w-full flex-1 flex-row flex-wrap justify-between gap-2">
         {question.options.map((option) => (
-          <ImageOption key={option.id} image={option.image} text={option.text} />
+          <ImageOption
+            key={option.id}
+            image={option.image}
+            text={option.text}
+            isSelected={selected?.id === option.id}
+            onPress={() => setSelected(option)}
+          />
         ))}
       </View>
+
+      <CustomButton text="Patvirtinti" onPress={onButtonPress} disabled={!selected} />
     </SafeAreaView>
   );
 }
