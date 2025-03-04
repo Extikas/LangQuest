820655a396e7701bdf8758d6326178d2657de5f0
RafaelPil <rafik.pilumyan@gmail.com>
2025-03-04 21:45:32 +0200

    Diena 4 Klausimų viktorinos logika (Quiz Logic)

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Alert, StatusBar } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import questions from '~/assets/data/MultipleChoiceQuestions';
+import questions from '~/assets/data/AllQuestionsData';
 import MultipleChoiceQuestion from './MultipleChoiceQuestion';
-import EndedQuestion from './EndedQuestion';
+import EndedQuestion from '~/components/EndedQuestion';
+import { QuizQuestion } from '~/types';
 
 export default function Home() {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // Indekas, nurodo dabartini klausima
-  const [currentQuestion, setCurrentQuestion] = useState(questions[currentQuestionIndex]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState<QuizQuestion>(
+    questions[currentQuestionIndex]
+  );
 
   useEffect(() => {
     if (currentQuestionIndex >= questions.length) {
-      // Parodo zinute, kai atsakyti visi klausimai
-      Alert.alert('Sveikiname! ', 'Jus atsakete i visus klausimus');
+      Alert.alert('Jūs laimėjote!');
       setCurrentQuestionIndex(0);
     } else {
-      // Nustatome nauja klausima
       setCurrentQuestion(questions[currentQuestionIndex]);
     }
   }, [currentQuestionIndex]);
@@ -24,20 +25,30 @@ export default function Home() {
     setCurrentQuestionIndex(currentQuestionIndex + 1);
   };
 
-  const onWrongAnser = () => {
-    Alert.alert('Netiesa! ', 'Bandykite dar karta!');
+  const onWrongAnswer = () => {
+    Alert.alert('Neteisingas atsakymas, bandykite dar kartą!');
   };
 
   return (
-    <SafeAreaView className="flex flex-1 items-center justify-center p-3">
-      <StatusBar animated barStyle={'light-content'} />
-
-      {/* <MultipleChoiceQuestion
-        question={currentQuestion}
-        onCorrectAnswer={onCorrectAnswer}
-        onWrongAnser={onWrongAnser}
-      /> */}
-      <EndedQuestion />
+    <SafeAreaView className="flex flex-1 p-3">
+      <StatusBar animated barStyle={'default'} />
+      {currentQuestion.type === 'MULTIPLE_CHOICE' && (
+        <MultipleChoiceQuestion
+          question={{
+            question: currentQuestion.text,
+            options: currentQuestion.options || [],
+          }}
+          onCorrectAnswer={onCorrectAnswer}
+          onWrongAnswer={onWrongAnswer}
+        />
+      )}
+      {currentQuestion.type === 'OPEN_ENDED' && (
+        <EndedQuestion
+          question={currentQuestion}
+          onCorrectAnswer={onCorrectAnswer}
+          onWrongAnswer={onWrongAnswer}
+        />
+      )}
     </SafeAreaView>
   );
 }
--- types.tsx
+++ types.tsx
@@ -5,11 +5,25 @@ export type Option = {
   correct?: boolean;
 };
 
+export type QuizQuestion = {
+  id: string;
+  type: 'OPEN_ENDED' | 'MULTIPLE_CHOICE';
+  text: string;
+  answer?: string;
+  options?: Option[];
+};
+
 export type QuestionProp = {
   question: {
     question: string;
     options: Option[];
   };
   onCorrectAnswer: () => void;
-  onWrongAnser: () => void;
+  onWrongAnswer: () => void;
+};
+
+export type EndedQuestionProps = {
+  question: QuizQuestion;
+  onCorrectAnswer: () => void;
+  onWrongAnswer: () => void;
 };
