b7900e81d704d2b74fc1cb8192514bc29495e1b8
Your Name <you@example.com>
2025-05-13 18:50:37 +0300

    README ir google

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,23 @@
 import '../global.css';
+import { ClerkProvider } from '@clerk/clerk-expo'
 
 import { Stack } from 'expo-router';
 
+const clerkApiKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!clerkApiKey) {
+  throw new Error('clerk api raktas nerastas');
+}
+
 export default function Layout() {
   return (
-    <Stack>
+    <ClerkProvider publishableKey={clerkApiKey}>
+      <Stack>
       <Stack.Screen name='index' options ={{ headerShown: false}} />
+      <Stack.Screen name='GameScreen' options ={{ headerShown: false}} />
+      <Stack.Screen name='MainScreen' options ={{ headerShown: false}} />
     </Stack>
+    </ClerkProvider>
+    
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,73 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { Alert, StatusBar } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import questions from '~/assets/data/AllQuestionsData';
-import MultipleChoiceQuestion from './MultipleChoiceQuestion';
-import EndedQuestion from '~/components/EndedQuestion';
-import { QuizQuestion } from '~/types';
-import HeaderComponent from '~/components/HeaderComponent';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useAuth } from '@clerk/clerk-expo'
+import { Redirect } from 'expo-router';
 
-export default function Home() {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [currentQuestion, setCurrentQuestion] = useState<QuizQuestion>(
-    questions[currentQuestionIndex]
-  );
-  const [lives, setLives] = useState(5);
+const Home = () => {
 
-  useEffect(() => {
-    if (currentQuestionIndex >= questions.length) {
-      Alert.alert('Jūs laimėjote!');
-      setCurrentQuestionIndex(0);
-    } else {
-      setCurrentQuestion(questions[currentQuestionIndex]);
-    }
-  }, [currentQuestionIndex]);
+  const {isSignedIn, isLoaded} = useAuth();
 
-  const onCorrectAnswer = () => {
-    setCurrentQuestionIndex(currentQuestionIndex + 1);
-  };
+  if(!isLoaded){
+    return <Text>loaded....</Text>
+  }
 
-  const onWrongAnswer = () => {
-    if (lives <= 1) {
-      Alert.alert('Jus pralaimejote!', 'Bandykite dar karta!', [
-        { text: 'Bandyti dar karta', onPress: restart },
-      ]);
-      setLives(0);
-    } else {
-      Alert.alert('Neteisingas atsakymas, bandykite dar kartą!');
-      setLives(lives - 1);
-    }
-  };
-
-  const restart = () => {
-    setLives(5);
-    setCurrentQuestionIndex(0);
-  };
-
-  return (
-    <SafeAreaView className="flex flex-1 p-3">
-      <StatusBar animated barStyle={'default'} />
-
-      {/* Header */}
-      <HeaderComponent progress={currentQuestionIndex / questions.length} lives={lives} />
-
-      {currentQuestion.type === 'MULTIPLE_CHOICE' && (
-        <MultipleChoiceQuestion
-          question={{
-            question: currentQuestion.text,
-            options: currentQuestion.options || [],
-          }}
-          onCorrectAnswer={onCorrectAnswer}
-          onWrongAnswer={onWrongAnswer}
-        />
-      )}
-      {currentQuestion.type === 'OPEN_ENDED' && (
-        <EndedQuestion
-          question={currentQuestion}
-          onCorrectAnswer={onCorrectAnswer}
-          onWrongAnswer={onWrongAnswer}
-        />
-      )}
-    </SafeAreaView>
-  );
+  if(isSignedIn) {
+    return <Redirect href={'/GameScreen'} />;
+  } else {
+    return <Redirect href={'/MainScreen'} />;
+  }
 }
+
+export default Home
